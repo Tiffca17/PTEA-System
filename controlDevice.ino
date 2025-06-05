7f9d794077ffb59e918b830e98e9425ef7ea9846
Tiffca17 <92198529+Tiffca17@users.noreply.github.com>
2025-06-05 08:30:59 -0500

    Update controlDevice.ino

--- controlDevice.ino
+++ controlDevice.ino
@@ -12,39 +12,39 @@ XBee xbee = XBee();
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
-String crusherStatus = "On";
-String screenStatus = "On";
+String crusherStatus = "Off";
+String screenStatus = "Off";
 
-uint8_t crusherStoppedFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 50 FF FE 00 00 43 53 3C  CD to CMS Alerts "CS"
+uint8_t crusherStoppedFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 5F FF FE 00 00 43 53 2D  CD to CMS Alerts "CS"
   0x7E, 0x00, 0x10, 0x10, 0x01,
-  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x50,
+  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x5F,
   0xFF, 0xFE, 0x00, 0x00,
   0x43, 0x53,
-  0x3C
+  0x2D
 }; 
 
-uint8_t crusherRestartFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 50 FF FE 00 00 43 52 3D  CD to CMS Alerts "CR"
+uint8_t crusherRestartFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 5F FF FE 00 00 43 52 2E  CD to CMS Alerts "CR"
   0x7E, 0x00, 0x10, 0x10, 0x01,
-  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x50,
+  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x5F,
   0xFF, 0xFE, 0x00, 0x00,
   0x43, 0x52,
-  0x3D
+  0x2E
 }; 
 
-uint8_t screenStoppedFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 50 FF FE 00 00 53 53 2C  CD to CMS Alerts "SS"
+uint8_t screenStoppedFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 5F FF FE 00 00 53 53 1D  CD to CMS Alerts "SS"
   0x7E, 0x00, 0x10, 0x10, 0x01,
-  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x50,
+  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x5F,
   0xFF, 0xFE, 0x00, 0x00,
   0x53, 0x53,
-  0x2C
+  0x1D
 }; 
 
-uint8_t screenRestartFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 50 FF FE 00 00 53 52 2D  CD to CMS Alerts "SR"
+uint8_t screenRestartFrame[] = {  //7E 00 10 10 01 00 13 A2 00 42 3E 9A 5F FF FE 00 00 53 52 1E  CD to CMS Alerts "SR"
   0x7E, 0x00, 0x10, 0x10, 0x01,
-  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x50,
+  0x00, 0x13, 0xA2, 0x00, 0x42, 0x3E, 0x9A, 0x5F,
   0xFF, 0xFE, 0x00, 0x00,
   0x53, 0x52,
-  0x2D
+  0x1E
 }; 
 
 void setup() {
@@ -57,17 +57,21 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
 
-  Serial.println("Waiting for XBee data...");
+  // Serial.println("Waiting for XBee data...");
 
   lcd.init();
   lcd.backlight();
 }
 
 void loop() {
+
   lcd.setCursor(3, 0);
   lcd.print("Control Device      ");  // pad with spaces to clear line
   lcd.setCursor(0, 1);
-  lcd.print(crusherStatus + "         ");      // pad with spaces to clear line
+  lcd.print("Crusher:" + crusherStatus + "      ");
+  lcd.setCursor(0, 2);
+  lcd.print("Screen:" + screenStatus + "      ");
+  // lcd.print(crusherStatus + "         ");      // pad with spaces to clear line
 
   xbee.readPacket();
 
@@ -89,40 +93,40 @@ void loop() {
       Serial.println(receivedData);
 
       if (receivedData == "CS") {
-        digitalWrite(7, HIGH);
+        digitalWrite(8, HIGH);
         crusherStatus = "Off";
         XBeeSerial.write(crusherStoppedFrame, sizeof(crusherStoppedFrame));
         lcd.setCursor(0, 1);
-        lcd.print(crusherStatus + "         ");
-        digitalWrite(9,HIGH);
+        lcd.print("Crusher:" + crusherStatus + "      ");
+        digitalWrite(10,HIGH);
         delay(2000);
-        digitalWrite(9,LOW);
+        digitalWrite(10,LOW);
       }
       else if (receivedData == "CR") {
-        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
         crusherStatus = "On";
         XBeeSerial.write(crusherRestartFrame, sizeof(crusherRestartFrame));
         lcd.setCursor(0, 1);
-        lcd.print(crusherStatus + "         ");
+        lcd.print("Crusher:" + crusherStatus + "      ");
       }
       else if (receivedData == "SS") {
         digitalWrite(7, HIGH);
-        digitalWrite(8, HIGH);
+        // digitalWrite(8, HIGH);
         screenStatus = "Off";
         XBeeSerial.write(screenStoppedFrame, sizeof(screenStoppedFrame));
-        lcd.setCursor(0, 1);
-        lcd.print(screenStatus + "         ");
-        digitalWrite(9,HIGH);
+        lcd.setCursor(0, 2);
+        lcd.print("Screen:" + screenStatus + "      ");
+        digitalWrite(10,HIGH);
         delay(2000);
-        digitalWrite(9,LOW);
+        digitalWrite(10,LOW);
       }
       else if (receivedData == "SR") {
         digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
+        // digitalWrite(8, LOW);
         screenStatus = "On";
         XBeeSerial.write(screenRestartFrame, sizeof(screenRestartFrame));
-        lcd.setCursor(0, 1);
-        lcd.print(screenStatus + "         ");
+        lcd.setCursor(0, 2);
+        lcd.print("Screen:" + screenStatus + "      ");
       }
     }
     else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
